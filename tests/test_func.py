f7281902913f49bf4b0446678f1081ade267834d
Marcel Dio <marceldio@gmail.com>
2024-04-06 00:24:18 +0200

    added mask_card_or_account, test_mask_card_or_account

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,4 +1,4 @@
-from utils.func import get_load_data, get_change_date_format
+from utils.func import get_load_data, get_change_date_format, mask_card_or_account
 import os
 import pytest
 from config import ROOT_DIR
@@ -16,3 +16,8 @@ def test_get_change_date_format():
     """Тест функции которая преобразует формат даты"""
     assert get_change_date_format("2019-02-01T00:00:00.000001") == "01.02.2019"
 
+
+def test_mask_card_or_account():
+    """Тест функции которая маскирует номер карты или счета."""
+    assert mask_card_or_account("Maestro 4598300720424501") == "Maestro 4598 30** **** 4501"
+    assert mask_card_or_account("Счет 27248529432547658655") == "Счет **8655"
--- utils/func.py
+++ utils/func.py
@@ -18,5 +18,18 @@ def get_change_date_format(date):
 
     return datetime.fromisoformat(date).strftime('%d.%m.%Y')
 
+
+def mask_card_or_account(value):
+  """
+  Функция маскирует номер карты или счета.
+  """
+  nums = value.split()
+  if nums[0] == "Счет":
+    return "Счет **" + value[-4:]
+  else:
+    card_name = ' '.join(nums[:-1])
+    return card_name + ' ' + nums[-1][:4] + ' ' + nums[-1][4:6] + '** **** ' + nums[-1][-4:]
+
 print(get_load_data(operations_file))
 print(get_change_date_format("2019-02-01T00:00:00.000001"))
+print(mask_card_or_account("Maestro 4598300720424501"))
