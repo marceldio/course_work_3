19f62eb43f2db4838a5f00fdffa39c422b0bb8a1
Marcel Dio <marceldio@gmail.com>
2024-04-06 05:35:42 +0200

    added print_last_operations

--- utils/func.py
+++ utils/func.py
@@ -7,15 +7,17 @@ operations_file = os.path.join(ROOT_DIR, 'utils', 'operations.json')
 
 
 def get_load_data(file_name):
-    """Функция возвращает список, загружая и читая файл с транзакциями"""
-
+    """
+    Функция возвращает список, загружая и читая файл с транзакциями
+    """
     with open(file_name, 'r') as f:
         return json.load(f)
 
 
 def get_change_date_format(date):
-    """Функция возвращает преобразованную дату в нужный формат"""
-
+    """
+    Функция возвращает преобразованную дату в нужный формат
+    """
     return datetime.fromisoformat(date).strftime('%d.%m.%Y')
 
 
@@ -30,34 +32,46 @@ def mask_card_or_account(value):
     card_name = ' '.join(nums[:-1])
     return card_name + ' ' + nums[-1][:4] + ' ' + nums[-1][4:6] + '** **** ' + nums[-1][-4:]
 
-operations = get_load_data(operations_file)
+
 def get_filtered(operations):
     """
     Функция возвращает отфильтрованные транзакции по статусу - Выполнено
     """
     executed_operations = [op for op in operations if op.get("state") == "EXECUTED"]
-
     return executed_operations
 
 
-sorting_file = get_filtered(operations)
 def get_sort_date(sorting_file):
-    """Функция возвращает транзакции сортированные по дате в обратном порядке"""
-
+    """
+    Функция возвращает транзакции сортированные по дате в обратном порядке
+    """
     executed_sorted = sorted(sorting_file, key=lambda op: datetime.fromisoformat(op['date']), reverse=True)
-
     return executed_sorted
 
 
 def get_format_summ(money):
-    """Функция возвращает сумму и валюту транзакции"""
+    """
+    Функция возвращает сумму и валюту транзакции
+    """
     amount = float(money['operationAmount']['amount'])
     currency_name = money['operationAmount']['currency']["name"]
     return f'{amount} {currency_name}'
 
 
-print(get_load_data(operations_file))
-print(get_change_date_format("2019-02-01T00:00:00.000001"))
-print(mask_card_or_account("Maestro 4598300720424501"))
-print(get_filtered(operations))
-print(get_sort_date(sorting_file))
+def print_last_operations():  # pragma: nocover
+    """
+    Основная функция - выводит детали последних 5 транзакций.,
+    """
+    loaded_file = get_load_data(operations_file)
+    filtered = get_filtered(loaded_file)
+    sorting = get_sort_date(filtered)
+
+    for i, operation in enumerate(sorting[:5]):
+        if i > 0:
+            print()
+
+        print(get_change_date_format(operation["date"]), operation["description"])
+        if operation["description"] != "Открытие вклада":
+            print(mask_card_or_account(operation["from"]) + " -> ", end="")
+        print(mask_card_or_account(operation["to"]))
+        print(get_format_summ(operation))
